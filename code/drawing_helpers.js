4a029b00a6f1ec912504465d083649f5d5f80977
jamesholdenmusic <94533377+jamesholdenmusic@users.noreply.github.com>
2024-04-26 13:26:23 +0100

    tidyup

--- code/drawing_helpers.js
+++ code/drawing_helpers.js
@@ -195,7 +195,7 @@ function parameter_button(p){
 	var pv = voice_parameter_buffer.peek(1,MAX_PARAMETERS*paramslider_details[p][11]+paramslider_details[p][9]);
 	var statecount = (paramslider_details[p][17].length - 1) / 2;
 	var pv2 = Math.floor(pv * statecount * 0.99999) * 2  + 1;
-	post("\ndrawing param button, values", statecount, pv, MAX_PARAMETERS*paramslider_details[p][8]+paramslider_details[p][9], paramslider_details[p][17], pv2, paramslider_details[p][17][pv2])
+	//post("\ndrawing param button, values", statecount, pv, MAX_PARAMETERS*paramslider_details[p][8]+paramslider_details[p][9], paramslider_details[p][17], pv2, paramslider_details[p][17][pv2])
 	draw_button(paramslider_details[p][0],paramslider_details[p][1],paramslider_details[p][2],paramslider_details[p][3],paramslider_details[p][4],paramslider_details[p][5],paramslider_details[p][6],paramslider_details[p][7], paramslider_details[p][17][pv2],pv);
 	mouse_click_values[paramslider_details[p][7]] = [paramslider_details[p][17][0],paramslider_details[p][17][pv2+1], MAX_PARAMETERS*paramslider_details[p][8]+paramslider_details[p][9], (pv+(1/statecount)) % 1];
 }
