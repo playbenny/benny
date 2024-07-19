352ff327ebd1e77ec72ac5b713b4060e084cdf67
jamesholdenmusic <94533377+jamesholdenmusic@users.noreply.github.com>
2024-07-19 17:01:41 +0100

    fix #440

--- code/display_pages.js
+++ code/display_pages.js
@@ -1080,7 +1080,8 @@ function draw_block(i){ //i is the blockno, we've checked it exists before this 
 		subvoices = blocks.get("blocks["+i+"]::subvoices");
 	} 
 	if(block_x<blocks_page.leftmost) blocks_page.leftmost = block_x;
-	if(block_x>blocks_page.rightmost) blocks_page.rightmost = block_x;
+	var block_x2 = block_x + block_v * 0.5;
+	if(block_x2>blocks_page.rightmost) blocks_page.rightmost = block_x2;
 	if(block_y<blocks_page.lowest) blocks_page.lowest = block_y;
 	if(block_y>blocks_page.highest) blocks_page.highest = block_y;
 	
