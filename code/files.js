96608c1e638fc1c46c10094c9b0d4a4df8f63198
James Holden <94533377+jamesholdenmusic@users.noreply.github.com>
2024-10-08 16:51:52 +0100

    fix #531

--- code/files.js
+++ code/files.js
@@ -1340,6 +1340,7 @@ function check_its_safe_to_save_named(){
 	if(loading.save_waitlist.length == 0){
 		post("\nall store routines complete, finalising save");
 		messnamed("save_named",loading.songpath+loading.songname);
+		for(var i =0;i<MAX_BLOCKS;i++) if(record_arm[i]) send_record_arm_messages(i); //update filenames of audio recorders
 	}else{
 		post("\nnot ready to save yet, waiting..");
 		savetask.schedule(1000);
