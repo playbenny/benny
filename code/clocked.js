a61362f903b041d836820b3149242adbc6968511
James Holden <94533377+jamesholdenmusic@users.noreply.github.com>
2025-02-23 02:48:50 +0000

    Update clocked.js

--- code/clocked.js
+++ code/clocked.js
@@ -89,7 +89,6 @@ function frameclock(){
 			redraw_flag.matrices |= 1;
 		}
 	}
-	if(redraw_flag.flag) post("\nflag",redraw_flag.flag);
 	if(redraw_flag.flag & 4){
 		redraw(); //redraw does everything 2 does + blocks, panels or custom or whatever
 		bangflag=1;
