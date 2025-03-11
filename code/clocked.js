b1ea0e414e23063c2bacc9fbdb47d6aa07b515c9
James Holden <94533377+jamesholdenmusic@users.noreply.github.com>
2025-03-11 19:09:14 +0000

    Update clocked.js

--- code/clocked.js
+++ code/clocked.js
@@ -22,6 +22,7 @@ function slowclock(){
 		deferred_diag=[];
 	}
 	draw_cpu_meter(); //is this the right place for this?
+	if(fullscreen && ((displaymode=="blocks")||(displaymode=="panels"))) draw_clock();
 }
 
 function frameclock(){
